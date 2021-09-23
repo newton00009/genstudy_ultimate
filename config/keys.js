bf9269bb61d90712ea28c661443327c7d9358851
Prasham Bhagwat <bhagwatprasham@yahoo.com>
2021-09-23 18:30:19 +0530

    Changes in Map and Added course names

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
+  mongoURI: 'mongodb+srv://nilesh:nilesh@cluster0.q0xrj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: "secret"
+};
