ce4f25bbf6e7d629157f09c0182d568ce4881c9c
Cheesy-Krish <krishnansh13@gmail.com>
2021-09-24 11:55:40 +0530

    date for courses

--- client/src/blog/VideoDetail.js
+++ client/src/blog/VideoDetail.js
@@ -1,21 +1,26 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-  if (!video) {
-    return <div>Loading....</div>;
-  }
+    if (!video) {
+        return <div > Loading.... < /div>;
+    }
 
-  //const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+    //const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
-  return (
-    <div>
-      <div className="cta-video-image">
-        <div className="ui embed">
-          <iframe src= {video.videoLink} title="videoplayer" />
-        </div>
-      </div>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        div className = "cta-video-image" >
+        <
+        div className = "ui embed" >
+        <
+        iframe src = { video.videoLink }
+        title = "videoplayer" / >
+        <
+        /div> <
+        /div> <
+        /div>
+    );
 };
 
 export default VideoDetail;
