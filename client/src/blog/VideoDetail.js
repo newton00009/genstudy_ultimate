bc0aff36c4d4e7c4acd4db20ba71e2b5cc5cdd25
Cheesy-Krish <krishnansh13@gmail.com>
2021-09-24 12:06:23 +0530

    error videodetail.js

--- client/src/blog/VideoDetail.js
+++ client/src/blog/VideoDetail.js
@@ -5,8 +5,22 @@ const VideoDetail = ({ video }) => {
         return <div > Loading.... < /div>;
     }
 
-<<<<<<< HEAD
-    //const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+    //const videoSrc = https://www.youtube.com/embed/${video.id.videoId};
+    //console.log(video.videoLink)
+
+
+
+    function YouTubeGetID(url) {
+        url = url.split(/(vi/ | v = | /v/ | youtu.be / | /embed/) / );
+    return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_-]/i)[0] : url[0];
+}
+
+
+
+const link = YouTubeGetID(video.videoLink)
+const videoSrc = https: //www.youtube.com/embed/${link};
+
+
 
     return ( <
         div >
@@ -15,41 +29,15 @@ const VideoDetail = ({ video }) => {
         <
         div className = "ui embed" >
         <
-        iframe src = { video.videoLink }
+        iframe width = "420"
+        height = "315"
+        src = { videoSrc }
         title = "videoplayer" / >
         <
         /div> <
         /div> <
         /div>
     );
-=======
-  //const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
-  //console.log(video.videoLink)
-
-
-
-  function YouTubeGetID(url){
-    url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-    return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_\-]/i)[0] : url[0];
- }
-
-
-
- const link= YouTubeGetID(video.videoLink)
- const videoSrc = `https://www.youtube.com/embed/${link}`;
-
-
-
-  return (
-    <div>
-      <div className="cta-video-image">
-        <div className="ui embed">
-          <iframe width="420" height="315" src= {videoSrc} title="videoplayer" />
-        </div>
-      </div>
-    </div>
-  );
->>>>>>> 54eddcc3de1207bc4fb32d19e1dd63a2f4e02380
 };
 
 export default VideoDetail;
